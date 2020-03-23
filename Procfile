30efce9dc8dcf203abfbd5b172d76197e73f0905
Kevin Lueke <kevinlueke66@gmail.com>
2020-03-23 15:04:50 -0400

    Test for initial Hello app, Fix Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn flask_adventure_game/__init__:app
+web: gunicorn "flask_adventure_game:create_app()"
--- tests/test_flask_adventure_game.py
+++ tests/test_flask_adventure_game.py
@@ -3,3 +3,10 @@ import flask_adventure_game
 def test_import():
     assert flask_adventure_game
 
+def test_config():
+    assert not flask_adventure_game.create_app().testing
+    assert flask_adventure_game.create_app({'TESTING': True}).testing
+
+def test_hello(client):
+    response = client.get('/hello')
+    assert response.data == b'Hello, World!'
