b56c085dc34243864cc7f97784fa819eb8a21b0a
Kevin Lueke <kevinlueke66@gmail.com>
2020-03-24 14:39:44 -0400

    Create a Blueprint

--- flask_adventure_game/__init__.py
+++ flask_adventure_game/__init__.py
@@ -28,6 +28,12 @@ def create_app(test_config=None):
     @app.route('/')
     def index():
         return 'Hello, World!'
+    
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
 
