d5db0797b83180a6e4f7ec60eea72bb5ca24c723
Kevin Lueke <kevinlueke66@gmail.com>
2020-03-25 12:49:17 -0400

    Blueprints, templates, and tests for auth

--- flask_adventure_game/__init__.py
+++ flask_adventure_game/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -24,18 +24,15 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
+#Index
     @app.route('/')
-    def hello():
-        return 'Hello, World!'
-    
+    def index():
+        return render_template('index.html')
+
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
-
-    from . import db
-    db.init_app(app)
 
     return app
--- tests/test_flask_adventure_game.py
+++ tests/test_flask_adventure_game.py
@@ -6,7 +6,3 @@ def test_import():
 def test_config():
     assert not flask_adventure_game.create_app().testing
     assert flask_adventure_game.create_app({'TESTING': True}).testing
-
-def test_hello(client):
-    response = client.get('/')
-    assert response.data == b'Hello, World!'
